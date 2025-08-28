775d5770bcd6e561404b34c08bb7a2891d432eea
xinwang <1294600058@qq.com>
2025-08-28 17:16:55 +0800

    feat: 更新README，添加开发模式说明，修改主题ID，补充Shopify CLI使用指南

--- README.js
+++ README.js
@@ -1,6 +1,8 @@
+// themeKit 开发模式
+
 ```bash
 # 线上模板
-theme get --password=shptka_a8bc1f303349ec792567bf0fd9d8d69c --store="simon2-xinwang.myshopify.com" --themeid=152928092389
+theme get --password=shptka_a8bc1f303349ec792567bf0fd9d8d69c --store="simon2-xinwang.myshopify.com" --themeid=152928911589
 
 # shopify 线上直接更改，自动同步
 theme watch --allow-live 
@@ -10,6 +12,26 @@ theme watch
 
 # git 提交清除无需提交文件
 git add --renormalize .
+xinwang1997
+```;
+
+// 2. shopify cli 开发模式
+```bash
+# 安装
+npm install -g @shopify/cli@latest
+
+# 开发已经存在的指定主题 id
+shopify theme dev -t 152928911589 --store=simon2-xinwang.myshopify.com
+
+# 如果需要输入密码
+shopify 没有付费的时候，是密码限制访问，密码在-在线商店-偏好设置-密码-默认-yeetse
+这个项目密码是： 123456
+
+# 通过push命令将本地开发主题上传到商店，第一次上传不希望将模板作为publish（live）模板可以添加标识--unpublished实现
+shopify theme push --unpublished
+
+# 退出登录
+shopify auth logout
 
 ```;
 
@@ -18,7 +40,7 @@ fetch("/cart/clear.js", {
   method: "POST",
   headers: {
     Accept: "application/json",
-  },  
+  },
 })
   .then((res) => res.json())
   .then((data) => {
